c4a6005dbe165a8d4187a87b45321a9e6919d5ee
monicamarshall <61441459+monicamarshall@users.noreply.github.com>
2021-04-12 15:13:59 -0600

    Update settings.py
    removed generated secret_key

--- Bulkloader/settings.py
+++ Bulkloader/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xt5m-&60$$*zd489s*rrhzv=doe$38z7us92i82lh=3oi)^@ni'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
